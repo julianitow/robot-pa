b36fb5d95b22296fd66580f4de849581470bf1ca
julien-guillan <guillan.julien@live.com>
2021-04-12 20:37:05 +0200

    HTTP/s + WS/s server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,58 @@
 import express from 'express';
+import https from 'https';
+import http from 'http';
+import WebSocket from 'ws';
+import fs from 'fs';
 
-const app = express();
-const port = 3000;
+class App {
 
-app.get('/', (req, res) => {
-  res.send('Sample typescript-app');
-});
+  public readonly port: number = 3000;
+  public express: any;
+  public server: https.Server | http.Server;
 
-app.listen(port, () => {
-  console.log(`server is listening on ${port}`);
-}); 
+  constructor(secured: Boolean) {
+    this.startHttpsServer(secured);
+    this.startWssServer();
+  }
+
+  private startHttpsServer(secured: Boolean) {
+    this.express = express();
+
+    const router = express.Router();
+    router.all('/', (req, res) => res.send('Welcome buddy!'));
+    this.express.use('/', router);
+
+    //  Server creation starts here
+    if(secured) {
+      const key = fs.readFileSync('/home/pi/cert/privkey1.pem');
+      const cert = fs.readFileSync('/home/pi/cert/fullchain1.pem');
+      this.server = https.createServer({ key, cert }, this.express);
+    } else {
+      this.server = http.createServer(this.express);
+    }
+
+    this.server.listen(this.port, () => {
+      console.log(`Server is listening on port ${this.port}`);
+    });
+  }
+
+  private startWssServer() {
+    console.log('WSS Server started');
+    const wsServer = new WebSocket.Server({ server: this.server });
+    wsServer.on('connection', ws => {
+      console.log('New connection');
+      ws.on('message', msg => {
+        console.log('received:', msg);
+        ws.send('Well received ! Over.');
+      });
+    });
+  }
+}
+
+function main(): number {
+  const app = new App(true);
+
+  return 0;
+}
+
+main();
